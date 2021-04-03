3ff86ae02e2260408ed8bfca9bbd6fd88ca0bbaa
mirza fahmi <mirzafahmi7@gmail.com>
2021-04-03 09:03:17 +0800

    next

---
+++ index.js
@@ -0,0 +1,27 @@
+let weather = {
+  apiKeys: "021723dabd539dfab7d1d24f81cd2f58",
+  fetchWeather: function (cityName) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.apiKeys}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function(data) {
+    const { name } = data;
+    const { country } = data.sys;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    document.querySelector(".city").innerText = `${name}, ${country}`;
+    document.querySelector(".temp").innerText = `${(temp - 273).toFixed(2)}°C`
+    document.querySelector(".icon").src= `http://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector(".description").innerText = `${description}`;
+    document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+    document.querySelector(".wind").innerText = `Wind speed: ${speed}km/h`;
+  },
+  search: function () { this.fetchWeather(document.querySelector(".search-bar").value)}
+};
+
+document.querySelector(".search button").addEventListener("click", () => weather.search())
